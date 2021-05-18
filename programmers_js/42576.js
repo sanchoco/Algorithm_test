e9389c5124cc4873549ff1ed5f281e5aedb0847a
sanchoco <jguuun@gmail.com>
2021-05-18 09:28:52 +0900

    프로그래머스 완주하지 못한 선수 문제 추가

---
+++ programmers_js/42576.js
@@ -0,0 +1,20 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42576?language=javascript
+// 완주하지 못한 선수
+
+function solution(participant, completion) {
+	participant.sort()
+	completion.sort()
+	console.log(participant)
+	console.log(completion)
+	while (participant.length) {
+		let p = participant.pop()
+		let c = completion.pop()
+		if (p != c) {
+			return p
+		}
+	}
+}
+
+let participant = ["leo", "kiki", "eden"]
+let completion = ["eden", "kiki"]
+console.log(solution(participant, completion))
--- programmers_js/42840.js
+++ programmers_js/42840.js
@@ -1,3 +1,6 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42840
+// 모의고사
+
 function solution(answers) {
 	const select_one = [1, 2, 3, 4, 5];
 	const select_two = [2, 1, 2, 3, 2, 4, 2, 5]

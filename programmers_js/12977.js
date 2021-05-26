693e3a020eda20529aacee9a074970e8895ea6ed
sanchoco <jguuun@gmail.com>
2021-05-26 09:52:51 +0900

    프로그래머스 소수 구하기 문제 수정

--- programmers_js/12977.js
+++ programmers_js/12977.js
@@ -12,14 +12,11 @@ function isPrime(number) {
 
 function solution(nums) {
 	let count = 0;
-	for (let i = 0; i < nums.length - 2; i++){
-		for (let j = i + 1; j < nums.length - 1; j++){
-			for (let k = j + 1; k < nums.length; k++){
+	for (let i = 0; i < nums.length - 2; i++)
+		for (let j = i + 1; j < nums.length - 1; j++)
+			for (let k = j + 1; k < nums.length; k++)
 				if (isPrime(nums[i] + nums[j] + nums[k]))
 					count++;
-			}
-		}
-	}
 	return count;
 }
 

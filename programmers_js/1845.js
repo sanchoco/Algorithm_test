7b7a9e38193c5452ac6e314f19ba0c0919967eea
sanchoco <jguuun@gmail.com>
2021-05-27 09:46:40 +0900

    프로그래머스 폰켓몬 문제 추가

---
+++ programmers_js/1845.js
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/1845
+// 폰켓몬
+
+function solution(nums) {
+	let arr = [...(new Set(nums))]
+	let max = Math.floor(nums.length / 2)
+	let answer = max >= arr.length ? arr.length : max;
+	return answer;
+}
+
+console.log(solution([3,1,2,3]))
+console.log(solution([3,3,3,2,2,4]))
+console.log(solution([3,3,3,2,2,2]))
